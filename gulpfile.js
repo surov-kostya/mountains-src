bcf449a6b987420eb79463bd1fc6319d617dffbd
surov-kostya <31765707+surov-kostya@users.noreply.github.com>
2017-12-13 16:47:03 +0300

    first section layout

--- gulpfile.js
+++ gulpfile.js
@@ -5,36 +5,33 @@ var gulp = require('gulp');
 // --------------- SCSS -----------------
 
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-
 var px2rem = require('gulp-px2rem');
-
-var px2remOptions = {
-   replace: true
-};
-
-var postCssOptions = {
-   map: true  
-};
+var px2remOptions = {replace: true};
+var postCssOptions = {map: true};
 
 gulp.task('sass', function () {
  return gulp.src('./src/styles/main.scss')
+   .pipe(sourcemaps.init())
    .pipe(sass().on('error', sass.logError))
   //  .pipe(px2rem(px2remOptions, postCssOptions))
    .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
     }))
+   .pipe(sourcemaps.write())
    .pipe(gulp.dest('./build/assets/styles'));
-
 });
 
 // ---------------- PUG -------------------
 
 var pug = require('gulp-pug');
+var plumber = require('gulp-plumber');
 
 gulp.task('pug', function buildHTML() {
   return gulp.src('./src/templates/pages/*.pug')
+  .pipe(plumber())
   .pipe(pug({
     pretty: true 
   }))
@@ -63,6 +60,13 @@ gulp.task('svgSprite', function () {
    return gulp.src('src/icons/*.svg')
        .pipe(svgSprite({mode: "symbols"}))
        .pipe(gulp.dest("build/assets/icons"));
+});
+
+// ---------- FONTS just copy -------------
+
+gulp.task('copyFonts', ()=>{
+    return gulp.src('src/fonts/*.woff?')
+        .pipe(gulp.dest('build/assets/fonts'));
 });
 
 // ----------- BROWSER SYNC ---------------
@@ -99,8 +103,11 @@ gulp.task('watch', function () {
  gulp.watch('./src/images/**/*.{png,jpg,jpeg}', gulp.series('tinypng'));
  gulp.watch('./src/scripts/**/*.js', gulp.series('webpack'));
  gulp.watch('./src/icons/*.svg', gulp.series('svgSprite'));
+ gulp.watch('src/fonts/*.{woff, woff2}', gulp.series('copyFonts'));
 });
 
 // ---------------- DEFAULT ----------------
+
+gulp.task('start', gulp.parallel('sass', 'pug', 'webpack', 'svgSprite', 'copyFonts'));
 
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
