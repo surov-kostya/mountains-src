2b14a08f88960458489e9cf498ced0dd7887bc64
Surov Konstantin <surov.qostya@yandex.ru>
2018-01-03 01:31:52 +0300

    Complete flip on index.html

---
+++ src/scripts/index-panel.js
@@ -0,0 +1,13 @@
+const authBtn = document.querySelector('.index-section__auth-btn');
+const indexBtn = document.querySelector('.index-panel__btn_index');
+const indexCenter = document.querySelector('.index-section__center');
+
+authBtn.addEventListener('click', ()=>{
+    authBtn.classList.add('visually-hidden');
+    indexCenter.classList.add('flipper');
+});
+
+indexBtn.addEventListener('click', ()=>{
+    authBtn.classList.remove('visually-hidden');
+    indexCenter.classList.remove('flipper');
+});
