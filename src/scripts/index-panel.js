32e3ca5230c240d672cfd9c4ec09f2beeeac7b0f
Surov Konstantin <surov.qostya@yandex.ru>
2018-01-10 01:46:01 +0300

    Add preload spinner

--- src/scripts/index-panel.js
+++ src/scripts/index-panel.js
@@ -1,9 +1,15 @@
 const authBtn = document.querySelector('.index-section__auth-btn');
 const indexBtn = document.querySelector('.index-panel__btn_index');
 const indexCenter = document.querySelector('.index-section__center');
+const indexPanel = document.querySelector('.index-panel');
 
+
+window.onload = ()=>{
+    indexCenter.classList.add('flipInX');
+};
 
 if (authBtn !== null){
+
     authBtn.addEventListener('click', ()=>{
         authBtn.classList.add('visually-hidden');
         indexCenter.classList.add('flipper');
